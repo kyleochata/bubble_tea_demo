5454d60c01e7fa5913e3cb50224c5f3d9464615a
kyleochata <kyleochata@gmail.com>
2024-09-19 10:51:35 -0700

    Cmd tutorial finished

--- main.go
+++ main.go
@@ -2,80 +2,63 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-type model struct {
-	choices  []string
-	cursor   int
-	selected map[int]struct{} //holds what choices are selected
-}
+const url = "https://charm.sh/"
 
-func initModel() model {
-	return model{
-		choices:  []string{"Buy lettuce", "Buy celery", "Buy miso"},
-		selected: make(map[int]struct{}),
+type model_cmd struct {
+	status int
+	err    error
+}
+type statusMsg int
+type errMsg struct{ err error }
+
+func checkServer() tea.Msg {
+	client := &http.Client{Timeout: 10 * time.Second}
+	response, err := client.Get(url)
+	if err != nil {
+		return errMsg{err}
 	}
+	return statusMsg(response.StatusCode)
 }
-func (m model) Init() tea.Cmd {
-	return nil
+func (e errMsg) Error() string { return e.err.Error() }
+func (m model_cmd) Init() tea.Cmd {
+	return checkServer
 }
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m model_cmd) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-
+	case statusMsg:
+		m.status = int(msg)
+		return m, tea.Quit
+	case errMsg:
+		m.err = msg
+		return m, tea.Quit
 	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c", "q":
+		if msg.Type == tea.KeyCtrlC {
 			return m, tea.Quit
-		case "up", "k":
-			if m.cursor > 0 {
-				m.cursor--
-			} else {
-				m.cursor = len(m.choices) - 1
-			}
-		case "down", "j":
-			if m.cursor < len(m.choices)-1 {
-				m.cursor++
-			} else {
-				m.cursor = 0
-			}
-		case "enter", " ":
-			_, ok := m.selected[m.cursor]
-			if ok {
-				delete(m.selected, m.cursor)
-			} else {
-				m.selected[m.cursor] = struct{}{}
-			}
 		}
 	}
 	return m, nil
 }
-func (m model) View() string {
-	//header
-	s := "What should we buy at the market?\n\n"
-	for i, choice := range m.choices {
-		cursor := " " //no cursor
-		//is cursor pointing at this choice?
-		if m.cursor == i {
-			cursor = ">" //cursor!
-		}
-		//is choice selected?
-		checked := " "
-		if _, ok := m.selected[i]; ok {
-			checked = "x" //selected!
-		}
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+func (m model_cmd) View() string {
+	if m.err != nil {
+		return fmt.Sprintf("\nWE had some trouble: %v\n\n", m.err)
 	}
-	s += "\nPress ctrl+c or q to quit. \n"
-	return s
+	s := fmt.Sprintf("Checking %s...", url)
+	if m.status > 0 {
+		s += fmt.Sprintf("%d %s!", m.status, http.StatusText(m.status))
+	}
+	return "\n" + s + "\n\n"
 }
 
 func main() {
-	program := tea.NewProgram(initModel())
-	if _, err := program.Run(); err != nil {
-		fmt.Printf("There has been an error: %v", err)
+	if _, err := tea.NewProgram(model_cmd{}).Run(); err != nil {
+		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
 }
